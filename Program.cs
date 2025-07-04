4606c94d4891b552b84838595eb5e764b8bea268
zinine73 <zinine@naver.com>
2025-07-04 15:50:39 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, anyone!");
+        // 이거...테스트
+        //Solution0704(3, 2);
+        Console.WriteLine(Solution0704(10, 5));
+    }
+
+    public static int Solution0704(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 % num2;
+        return answer;
     }
 }
